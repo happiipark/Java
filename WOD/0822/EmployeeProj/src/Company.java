ba49969cacbcf322ffd06ae2cb0fa4b1f59ada7b
happiipark <124240328+happiipark@users.noreply.github.com>
2023-08-23 00:44:32 +0900

    숙제
    Company, Employee

--- WOD/0822/EmployeeProj/src/Company.java
+++ WOD/0822/EmployeeProj/src/Company.java
@@ -1,21 +1,69 @@
+package KOSTA._0822;
 
 public class Company {
-	
-	public static void main(String[] args) {
-		Company com = new Company();
 
-		Employee emp1 = new Employee(10001, "홍길동", "홍보부", 5000000);
-		Employee emp2 = new Employee(10002, "고길동", "총보부", 5000000);
-		Employee emp3 = new Employee(10003, "하길동", "개발부", 5000000);
+    Employee[] emp = new Employee[100];
+    static int empCnt = 0;
 
-		com.enter(emp1);
-		com.enter(emp2);
-		com.enter(emp3);
-		com.setBonus(10001, 1000000);
+    void enter(Employee emp) {
+        this.emp[empCnt++] = emp;
+    }
 
-		com.allEmployeeInfo();
-		System.out.println("총급여액 : " + com.getTotalPay());
-	}
+    void allEmployeeInfo() {
+        for (int i = 0; i < empCnt; i++) {
+            System.out.println(emp[i].info());
+        }
+    }
+
+
+    // 사번 찾기
+    Employee searchById(int id) {
+        Employee res = null;
+        for (int i = 0; i < empCnt; i++) {
+            if (emp[i].id == id) {
+                res = emp[i];
+                break; // break 를 걸어줘서 다음 반복이 진행되지 않게끔 원하는 사번 하나만 찾으면 되기 때문에!
+            } else {
+                res = null;
+            }
+        }
+        return res;
+    }
+
+    void setBonus(int id, int bonus) {
+        Employee emp = searchById(id);
+        if (emp == null) {
+            System.out.println("잘못된 사번을 입력하였습니다.");
+        } else {
+            emp.salary += bonus;
+        }
+    }
+
+    int getTotalPay() {
+        int totalSalary = 0;  // 총급여
+        for (int i = 0; i < empCnt; i++) {
+            totalSalary += emp[i].salary;
+        }
+        return totalSalary;
+    }
+
+    public static void main(String[] args) {
+
+        Company com = new Company();
+
+
+        Employee emp1 = new Employee(10001, "홍길동", "홍보부", 5000000);
+        Employee emp3 = new Employee(10003, "하길동", "개발부", 5000000);
+        Employee emp2 = new Employee(10002, "고길동", "총보부", 5000000);
+
+        com.enter(emp1);
+        com.enter(emp2);
+        com.enter(emp3);
+        com.setBonus(10001, 1000000);
+
+        com.allEmployeeInfo();
+        System.out.println("총급여액 : " + com.getTotalPay());
+    }  
 
 }
 
--- WOD/0822/EmployeeProj/src/Employee.java
+++ WOD/0822/EmployeeProj/src/Employee.java
@@ -1,9 +1,21 @@
+package KOSTA._0822;
 
 public class Employee {
-	int id;
-	String name;
-	String department;
-	int salary;
+    int id; // 계좌 번호
+    String name; // 이름
+    String department; // 부서
+    int salary; // 급여
 
-	
+    Employee(int id, String name, String department, int salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    String info() {
+        return String.format("사번 : %d, 이름 : %s, 부서 : %s, 급여 : %d",
+                                    id, name, department, salary);
+    }
 }
+
